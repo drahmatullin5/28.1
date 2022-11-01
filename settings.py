420f83d1ef0441a97e8469734a3d692b136da387
Dmitry <104850369+drahmatullin5@users.noreply.github.com>
2022-11-01 12:53:53 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 login_url = 'https://b2c.passport.rt.ru'
 valid_email = 'dimarahmatullin95@gmail.com'
-valid_password = '152152Fa'
+valid_password = '152152Qq'
 valid_phone_number = '+79182154261'
 valid_login = 'Ввести валидный логин' #дается только клиентам Ростелеком
 valid_account_number = 'Ввести валидный номер лицевого счета' #дается только клиентам Ростелеком
